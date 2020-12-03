af3ef9e7d050a1180876e91f34c44eeca68dd529
Milki Negeri <milki10mn@gmail.com>
2020-12-03 17:34:01 -0600

    modified LoadStopWords method

--- vsr/document.go
+++ vsr/document.go
@@ -1,9 +1,10 @@
 package vsr 
 
 import (
-   "bufio"
-    "os"
+   //"bufio"
+    //"os"
     "fmt"
+    "strings"
 )
 
 type Document struct {
@@ -11,19 +12,25 @@ type Document struct {
     StopWords map[string]bool
 }
 
+const stopwords = `a an and are as at be by for from has he in is it its of on that the to was were will with`
+
 
 func (doc *Document) LoadStopWords() {
-    file, err := os.Open("vsr/stop_words.txt")
+    /*file, err := os.Open("vsr/stop_words.txt")
     if err != nil {
         panic(err)
-    }
+    } 
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         word := scanner.Text()
         fmt.Println(word)
         doc.StopWords[word] = true
     }
-    file.Close()
+    file.Close() */
+    words := strings.Fields(stopwords)
+    for _, word := range words {
+        doc.StopWords[word] = true
+    }
 }
 
 func (doc *Document) Test() {
--- vsr/document_test.go
+++ vsr/document_test.go
@@ -7,7 +7,7 @@ import (
 func TestLoadStopWords(t *testing.T) {
     doc := Document{"stop_words.txt", make(map[string]bool)}
     doc.LoadStopWords()
-    word, ok := doc.StopWords["and"]
+    _, ok := doc.StopWords["and"]
     if !ok {
         t.Errorf("stop word \"and\" is not present")
     }
