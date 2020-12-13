84cda03e130a3b650da5423b535e5ec2e48d508f
Milki Negeri <milki10mn@gmail.com>
2020-12-13 00:44:46 -0600

    update main, will finish later

--- main.go
+++ main.go
@@ -5,25 +5,26 @@ import (
     "fmt"
     //"os"
     "github.com/mnegeri/search/vsr"
+   // "bufio"
 )
 
 func main() {
     fmt.Println("test")
     doc := vsr.Document{"hello", 0, make(map[string]bool)}
     doc.LoadStopWords()
-    fmt.Println(doc.StopWords["and"])
-    /*args := os.Args[1:]
+    fmt.Println(doc.StopWords["and"]) 
+    /* args := os.Args[1:]
     dir := args[len(args) - 1]
-    var stem bool
-    var html bool
-    for i := 0; i < len(args) - 1; i++ {
-        if args[i] == "-stem" {
-            stem := true
-        } 
-        if args[i] == "-html" {
-            html := true
-        } 
+    
+    cont := true
+    reader := bufio.NewReader(os.Stdin)
+    for cont {
+        fmt.Println("Enter search query: ")
+        query, _ := reader.ReadString('\n')
+        fmt.Println("Press y to continue: ")
+        resp, _ := reader.ReadString('\n')
+        if resp == "y\n" {
+            cont = false
+        }
     } */
- //   test := document.Document{"Milki"}
-  //  test.Test()
 } 
