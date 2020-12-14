3588ce75724c07e622ccdfdf6f425ac103e9f9f5
Milki Negeri <milki10mn@gmail.com>
2020-12-14 14:47:18 -0600

    updated main

--- main.go
+++ main.go
@@ -3,28 +3,31 @@ package main
 
 import ( 
     "fmt"
-    //"os"
+    "os"
     "github.com/mnegeri/search/vsr"
-   // "bufio"
+    "bufio"
 )
 
 func main() {
-    fmt.Println("test")
+    /* fmt.Println("test")
     doc := vsr.Document{"hello", 0, make(map[string]bool)}
     doc.LoadStopWords()
-    fmt.Println(doc.StopWords["and"]) 
-    /* args := os.Args[1:]
+    fmt.Println(doc.StopWords["and"]) */
+    args := os.Args[1:]
     dir := args[len(args) - 1]
-    
     cont := true
     reader := bufio.NewReader(os.Stdin)
+    index := vsr.InvertedIndex{dir, make([]vsr.Document, 0), make(map[string]vsr.TermData)}
+    index.ProcessDocuments()
     for cont {
         fmt.Println("Enter search query: ")
         query, _ := reader.ReadString('\n')
+        results := index.RetrieveDocs(&vsr.Document{"", query, 0, make(map[string]bool), 0})
+        vsr.PrintResults(results)
         fmt.Println("Press y to continue: ")
         resp, _ := reader.ReadString('\n')
         if resp == "y\n" {
             cont = false
         }
-    } */
+    } 
 } 
