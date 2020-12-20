8124122f04cc629810abbfbef031e3049ed224b9
Milki Negeri <milki10mn@gmail.com>
2020-12-20 17:39:23 -0600

    added comments to term.go

--- vsr/term.go
+++ vsr/term.go
@@ -1,12 +1,15 @@
 package vsr
 
 
+//TermData represents information on all instances  where a certain 
+//term occurs in the corpus and the computed idf value of that term.
 type TermData struct {
     InstanceList []TermInstance
     Idf float64 
 }
 
-
+//TermInstance represent a single instance of a term. Stores 
+//the document is occurs in and the number of times it occurs.
 type TermInstance struct {
     Doc Document
     Count float64 
